9f97ebf316ff295333f742b54d1969658fae9464
michelecanini <studio.michelecanini@gmail.com>
2023-06-04 16:55:17 +0200

    [UPDATE] optimize code

--- js/script.js
+++ js/script.js
@@ -5,13 +5,19 @@
 
 // DICHIARAZIONE DELLE VARIABILI
 
-let random_number = ' '; // dichiarazione contenitore numeri randomici
-let r_number_one = " " + "<br>";
-let r_number_two = " ";
-let r_number_three = " ";
-let r_number_four = " ";
-let r_number_five = " ";
+let random_number = []; // array contenitore numeri randomici
+console.log(random_number)
 
+// FUNZIONE - VERIFICA SE IL NUMERO E' GIA' PRESENTE NELL'ARRAY, SOLO SE NON E' PRESENTE LO AGGIUNGE
+function controlRandomNumber(array, number){
+    let check = false
+    while(check === false){
+        if(!array.includes(number)){ // se il numero da inserire è diverso dall'array 
+            check = true; // controllo vero
+        }
+    }    
+    return true; // restituisci vero
+}
 
 // DICHIARAZIONE DELLE FUNZIONI - LOGICA
 
@@ -22,94 +28,56 @@ return Math.floor(Math.random() * (max - min + 1) ) + min;
 
 // CICLO FOR CHE RIPETE 5 VOLTE IL CODICE
 for (let i = 0; i < 5; i++){
-    random_number += generateRandomNumber(1, 100) + "<br>";
+    let number = generateRandomNumber (1, 100);
+
+    if(controlRandomNumber(random_number,number)){
+
+        random_number.push(number);
+    }
 }
-console.log(typeof generateRandomNumber(1, 100))
 
 // INSERIMENTO NEL DOM HTML
 document.getElementById("random").innerHTML = random_number;
 
 // CREAZIONE DEL TIMER PER LA CANCELLAZIONE DEI NUMERI
-setTimeout(myFunctionNumber, 30000);
+setTimeout(function(){
 
-// FUNZIONE 1 - CANCELLAZIONE DEI NUMERI CON DISPLAY NONE
-function myFunctionNumber(){
-    // CANCELLAZIONE DEI NUMERI CON DISPLAY NONE
-    document.getElementById("random").style.display = "none";
+    document.getElementById("random").style.display = "none"; // style cancellazione dei numeri
+    document.getElementById("results").innerHTML = 'i 30 secondi sono scaduti, inserisci i numeri che ricordi';
+
+},30000);
+
+// FUNZIONE 3 - PERMETTE L'INSERIMENTO DEI NUMERI UTENTE
+function getUserNumber(){
+    let user_number = [];
+
+    while(user_number.length < 5){
+        let number = parseInt(prompt("Inserisci i numeri memorizzati, uno alla volta"));
+
+        if(!user_number.includes(number)){
+            user_number.push
+        }
+    }
+    return user_number;
 }
 
-// CREAZIONE DEL TIMER PER I PROMPT
-setTimeout(myFunctionTimer, 31000);
 
-// FUNZIONE 2 - CONTENENTE LE VARIABILI CON PROMPT
-function myFunctionTimer(){
+// CREAZIONE DEL TIMER PER I PROMPT - FUNZIONE 4 - CHE PERMETTE L'INSERIMENTO DEI NUMERI UTENTE
+setTimeout(function(){
 
     // DICHIARAZIONE DELLE VARIABILI CON PROMPT
-    alert("OK i 30 secondi a tua disposizione sono scaduti, per favore inserisci i 5 numeri che hai memorizzato");
-    let num1 = parseInt(prompt("Inserisci il primo numero memorizzato"));
-    let num2 = parseInt(prompt("Inserisci il secondo numero memorizzato"));
-    let num3 = parseInt(prompt("Inserisci il terzo numero memorizzato"));
-    let num4 = parseInt(prompt("Inserisci il quarto numero memorizzato"));
-    let num5 = parseInt(prompt("Inserisci il quinto numero memorizzato"));
-    console.log(typeof num1, num2, num3, num4, num5)
-
-    // IF DI CONTROLLO DEI VALORI
-    if (num1 === r_number_one){ 
-        document.getElementById("results_one").innerHTML = (`OK, sei riuscito a memorizzare il primo numero: ${r_number_one}`);
-    } else {
-        document.getElementById("results_one").innerHTML = (`N.1 ERRATO`);
-    }
+    let user_number = getUserNumber;
+    let correct_number = []; // numeri corretti
+    let score = 0; // punteggio utente dei numeri corretti
     
-    if (num2 === r_number_two){
-        document.getElementById("results_two").innerHTML = (`OK, sei riuscito a memorizzare il secondo numero: ${r_number_two}`);
-    } else {
-        document.getElementById("results_two").innerHTML = (`N.2 ERRATO`);
+    for(let i=0; i<user_number.length; i++){
+            if(random_number.includes(user_number[i])){
+            correct_number.push(user_number[i]);
+            score++;
+
+        }
+
     }
+    document.getElementByid('results').innerHTML = `Hai indovinato ${score} numeri. I numeri corretti sono ${correct_number}`;
 
-    if (num3 === r_number_three){
-        document.getElementById("results_three").innerHTML = (`OK, sei riuscito a memorizzare il terzo numero: ${r_number_three}`);
-    } else {
-        document.getElementById("results_three").innerHTML = (`N.3 ERRATO`);
-    }
-
-    if (num4 === r_number_four){
-        document.getElementById("results_four").innerHTML = (`OK, sei riuscito a memorizzare il quarto numero: ${r_number_four}`);
-    } else {
-        document.getElementById("results_four").innerHTML = (`N.4 ERRATO`);
-    }
-
-    if (num5 === r_number_five){
-        document.getElementById("results_five").innerHTML = (`OK, sei riuscito a memorizzare il quinto numero: ${r_number_five}`);
-    } else {
-        document.getElementById("results_five").innerHTML = (`N.5 ERRATO`);
-    }
-
-    if (num1 === r_number_one && num2 === r_number_two && num3 === r_number_three && num4 === r_number_four && num5 === r_number_five){
-        document.getElementById("results").innerHTML = (`HAI VINTO HAI MEMORIZZATO TUTTI I NUMERI!`);
-    } else { 
-        document.getElementById("results").innerHTML = (`NON HAI MEMORIZZATO TUTTI I NUMERI, RIPROVA.`); 
-    }
-} 
-
-/* (SUPERATO)  
-  document.getElementById("random_number_one").style.display = "none";
-    document.getElementById("random_number_two").style.display = "none";
-    document.getElementById("random_number_three").style.display = "none";
-    document.getElementById("random_number_four").style.display = "none";
-    document.getElementById("random_number_five").style.display = "none"; */
-
-/* // CREAZIONE DEI NUMERI RANDOM (SUPERATO)
-let r_number_one = parseInt(Math.floor(Math.random() * 100) + 1);
-let r_number_two = parseInt(Math.floor(Math.random() * 100) + 1);
-let r_number_three = parseInt(Math.floor(Math.random() * 100) + 1);
-let r_number_four = parseInt(Math.floor(Math.random() * 100) + 1);
-let r_number_five = parseInt(Math.floor(Math.random() * 100) + 1); 
-*/
-
-/* // INSERIMENTO NEL DOM DEI NUMERI CASUALI (SUPERATO)
-document.getElementById("random_number_one").innerHTML = r_number_one;
-document.getElementById("random_number_two").innerHTML = r_number_two;
-document.getElementById("random_number_three").innerHTML = r_number_three;
-document.getElementById("random_number_four").innerHTML = r_number_four;
-document.getElementById("random_number_five").innerHTML = r_number_five;
-*/
+},31000);
--- readme.md
+++ readme.md
@@ -1,23 +1,22 @@
 # JS-SIMON
 
-### PROBLEMA: Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+#### PROBLEMA: Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-### SOTTO-PROBLEMA 1: Visualizzare in pagina 5 numeri casuali.
-    1. creare 5 div con id in html
-    2. creare 5 numeri random in js
-    3. collegare i 5 numeri random al dom
+#### SOTTO-PROBLEMA 1: Visualizzare in pagina 5 numeri casuali.
+    1. creare un array
+    3. utilizzare una funzione per creare i 5 numeri random
     4. successivemente stilizzare in numeri in css
     5. crea un header con titolo per avvisare l'utente 
 
-### SOTTO-PROBLEMA 2: Da lì parte un timer di 30 secondi.
+#### SOTTO-PROBLEMA 2: Da lì parte un timer di 30 secondi.
     1. crea un timer con la timing function di javascript
 
-### SOTTO-PROBLEMA 3: Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt()
+#### SOTTO-PROBLEMA 3: Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt()
     1. far scomparire i numeri dopo la scadenza del timer (style.display = "none")
     2. crea una funzione per conternere allert e prompt da creare
     2. crea un allert per avvisare di inserire i numeri
     3. crea 5 prompt con altrettanti messaggi per inserire i numeri memorizzati dall'utente
 
-### SOTTO-PROBLEMA 4: il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+#### SOTTO-PROBLEMA 4: il software dice quanti e quali dei numeri da indovinare sono stati individuati.
     1. crea una funzione per il controllo di uguaglianza tra i numeri randomici e i numeri inseriti dall'utente
     2. stampa a video e segnala all'utente quanti e quali numeri è riuscito a memorizzare
